1affc40f23412ce2fe764b44e863c38c58b06518
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-09-21 10:51:42 +0100

    docs: specifications and import file csv of categagories

--- src/server.ts
+++ src/server.ts
@@ -18,30 +18,6 @@ server.use(express.json());
 
 expressSwagger(server);
 
-const options = {
-    swaggerDefinition: {
-        info: {
-            description: 'This is a sample server',
-            title: 'Swagger',
-            version: '1.0.0',
-        },
-        host: 'localhost:3336',
-        basePath: '/v1',
-        produces: ['application/json', 'application/xml'],
-        schemes: ['http', 'https'],
-        securityDefinitions: {
-            JWT: {
-                type: 'apiKey',
-                in: 'header',
-                name: 'Authorization',
-                description: '',
-            },
-        },
-    },
-    basedir: __dirname, // app absolute path
-    files: ['./routes/*.ts'], // Path to the API handle folder
-};
-
 server.use(Routes);
 
 server.use(
