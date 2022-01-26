ff1177b2741e69f2ea37a982260d23ad666c2e95
Samuel Paulo António <samueldev1997@gmail.com>
2022-01-27 00:30:25 +0100

    chore: remove space white

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -11,7 +11,8 @@ export default {
         destination: tmpFolder,
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(16).toString('hex');
-            const filename = `${fileHash}-${file.originalname}`;
+            const SpaceWhiteRemoved = file.originalname.replace(/\s/g, '');
+            const filename = `${fileHash}-${SpaceWhiteRemoved}`;
 
             return callback(null, filename);
         },
